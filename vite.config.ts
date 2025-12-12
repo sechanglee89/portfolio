6aeb7f170df2fec85cdcb840a461780991d7dd85
sechanglee <peerscc@gmail.com>
2025-12-12 17:43:41 +0900

    누락소스 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // GitHub Pages 배포 시 리포지토리 이름으로 변경
+  base: "/portfolio/", // GitHub Pages 배포 시 리포지토리 이름으로 변경
   plugins: [react()],
   resolve: {
     alias: {
